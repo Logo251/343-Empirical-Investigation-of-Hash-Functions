e929f6d4f7d56f277ac679eac3d9e5aac400da69
Logo251 <61099371+Logo251@users.noreply.github.com>
2020-02-22 17:36:22 -0800

    Improved performance of hash printing when printing an empty hash, fixed bug where AddUsingName was adding using number, added documentation.

--- Hash.cpp
+++ Hash.cpp
@@ -5,12 +5,16 @@ Hash::Hash() {
    
    //Assign every element of the array to nullptr.
    for (int i = 0; i < 4177; i++) { //4177 is the size of the hash table.
+      //Every data point inside hashTable has not been interated through yet.
+
+      //Assign it to nullptr.
       hashTable[i] = nullptr;
    }
 }
 
 Hash::~Hash() {
    for (Node* i : hashTable) {
+      //Every data point inside hashTable has not been interated through yet.
       if (i != nullptr) {
          if (i->nextNode != nullptr) {
             delete i;
@@ -20,6 +24,7 @@ Hash::~Hash() {
             Node* nextNodeStorage = thisNode->nextNode;
 
             while (nextNodeStorage != nullptr) {
+               //Every Node inside this bin has not been iterated through yet.
                delete thisNode;
                nextNodeStorage = thisNode;
                nextNodeStorage = thisNode->nextNode;
@@ -32,62 +37,65 @@ Hash::~Hash() {
 void Hash::AddUsingNumber(std::string inputName, int inputNumbers[]) {
 
    //Local Variables
-   long long int addedNumbers = 0;
-   long long int phoneNumber = 0;
+   long long int hashPosition = 0;  //Where this name and phone number should be placed. Can be any non-negative value.
+   long long int phoneNumber = 0;   //Stores the phone number in int form for math purposes. Can be any non-negative value.
 
    //Get the phone number in int form.
    phoneNumber = ConvertIntArrayToInt(inputNumbers);
-   addedNumbers = phoneNumber;
+   hashPosition = phoneNumber;
 
-   assert(addedNumbers >= 0); //should never be negative.
+   assert(hashPosition >= 0 && phoneNumber >= 0); //should never be negative.
 
-   addedNumbers %= 4177; //4177 is the size of the hash table.
+   hashPosition %= 4177; //4177 is the size of the hash table.
 
-   AddToHashTable(inputName, phoneNumber, addedNumbers);
+   AddToHashTable(inputName, phoneNumber, hashPosition);
 }
 
 void Hash::AddUsingName(std::string inputName, int inputNumbers[]) {
 
    //Local Variables
-   long long int addedNumbers = 0;
-   long long int phoneNumber;
+   long long int hashPosition = 0;  //Where this name and phone number should be placed. Can be any non-negative value.
+   long long int phoneNumber = 0;   //Stores the phone number in int form for math purposes. Can be any non-negative value.
 
    //Convert to Ascii value of the character.
    for(char i : inputName) {
-      addedNumbers += (int)i;
+      //Every character in the string has not been iterated through yet.
+
+      hashPosition += (int)i;
    }
 
    //Get the phone number in int form.
    phoneNumber = ConvertIntArrayToInt(inputNumbers);
-   addedNumbers = phoneNumber;
 
-   assert(addedNumbers >= 0); //should never be negative.
+   assert(hashPosition >= 0 && phoneNumber >= 0); //should never be negative.
 
-   addedNumbers %= 4177; //4177 is the size of the hash table.
+   hashPosition %= 4177; //4177 is the size of the hash table.
 
-   AddToHashTable(inputName, phoneNumber, addedNumbers);
+   AddToHashTable(inputName, phoneNumber, hashPosition);
 }
 
 void Hash::AddUsingNameAndNumber(std::string inputName, int inputNumbers[]) {
 
    //Local Variables
-   long long int addedNumbers = 0;
-   long long int phoneNumber = 0;
+   long long int hashPosition = 0;  //Where this name and phone number should be placed. Can be any non-negative value.
+   long long int phoneNumber = 0;   //Stores the phone number in int form for math purposes. Can be any non-negative value.
 
    //Get the phone number in int form.
    phoneNumber = ConvertIntArrayToInt(inputNumbers);
-   addedNumbers = phoneNumber;
+   hashPosition = phoneNumber;
 
-   //Convert to Ascii value of the character.
+   //Convert to Ascii value of the character and add it to the phonenumber.
    for (char i : inputName) {
-      addedNumbers += (int)i;
+      //Every character in the string has not been iterated through yet.
+
+      hashPosition += (int)i;
    }
 
-   assert(addedNumbers >= 0); //should never be negative.
+   assert(hashPosition >= 0 && phoneNumber >= 0); //should never be negative.
 
-   addedNumbers %= 4177; //4177 is the size of the hash table.
+   hashPosition %= 4177; //4177 is the size of the hash table.
 
-   AddToHashTable(inputName, phoneNumber, addedNumbers);
+   AddToHashTable(inputName, phoneNumber, hashPosition);
 }
 
 long long int Hash::ConvertIntArrayToInt(int inputArray[]) {
@@ -97,7 +105,11 @@ long long int Hash::ConvertIntArrayToInt(int inputArray[]) {
 
    //Convert phone number array to phone number.
    for (int i = 0; i < 10; i++) {   //10 is the number of digits in a phone number.
+      //Every digit in the phone number array inputArray[] has not been iterated through yet.
+
       phoneNumber += inputArray[i];
+
+      //This is used to add a new place for another digit.
       phoneNumber *= 10;
    }
    phoneNumber /= 10; //Remove the last x10, so we have a 10 digit phone number.
@@ -111,38 +123,55 @@ void Hash::AddToHashTable(std::string name, long long int phoneNumber, long long
    //Local Variables
    Node* chainLink = new Node(name, phoneNumber);
 
+   //When there is an open spot.
    if (hashTable[position] == nullptr) {
       hashTable[position] = chainLink;
    }
+   //Iterate down the chain existing in this bin, put new node at the end.
    else {
       Node* endNode = hashTable[position];
       while (endNode->nextNode != nullptr) {
+         //The program is not at the end of the chain yet.
+
          endNode = endNode->nextNode;
       }
       endNode->nextNode = chainLink;
    }
+
+   //Set filled to true, so operator<< knows that it has information to print.
+   filled = true;
 }
 
 std::ostream& operator<<(std::ostream& out, const Hash& hash) {
 
    //Local Variables
-   int longestChain = 0;
-   int chainSize = 0;
-   int countOfChains = 0; //This is the number of chains with a certain length.
-   Hash::Node* chainLink;
-   int lengthStorage[4177] = { 0 }; //Stores the lengths of each bin's chain.
+   int longestChain = 0;   //This is storage of the longest chain. Valid range is all non-negative numbers.
+   int chainSize = 0;      //This is the size of the current chain in question. Valid range is all non-negative numbers.
+   int countOfChains = 0;  //This is the number of chains with a certain length. Valid range is all non-negative numbers.
+   Hash::Node* chainLink;  //This is used to point to a chain node so we can iterate down a chain to count.
+   int lengthStorage[4177] = { 0 }; //Stores the lengths of each bin's chain. Each digit's valid range is any non-negative number.
+
+   //If nothing is in hash, no need to attempt to print.
+   if (!hash.filled) {
+      std::cout << "Nothing was in the file.\n";
+      return out;
+   }
 
    //This takes the length of each chain, and puts the value in another array in the same position as the other.
    for(int i = 0; i < 4177; i++) { //4177 is the size of the hash table.
+      //Every bin inside hashTable has not been interated through yet.
+
       chainLink = hash.hashTable[i];
       if(chainLink != nullptr) {
          chainSize++; //count the first node.
          while(chainLink->nextNode != nullptr) {
+            //Every Node inside this bin has not been iterated through yet.
+
             chainSize++;
             chainLink = chainLink->nextNode;
          }
-         if (i == 480) {
-         }
+         
+         //If new chain is longer than previous best, replace.
          if(longestChain < chainSize) {
             longestChain = chainSize;
          }
@@ -153,7 +182,11 @@ std::ostream& operator<<(std::ostream& out, const Hash& hash) {
 
    //Counts the number of each chain length, and prints it.
    for(int i = 1; i <= longestChain; i++) {
+      //We have not iterated through every possible chain length.
+
       for(int j = 0; j < 4177; j++) {
+         //We have not iterated through every bin in lengthStorage.
+
          if(lengthStorage[j] == i) {
             countOfChains++;
          }
