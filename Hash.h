9ab3eed409362b0b27b9227959d0191a6a7fb0b2
Logo251 <61099371+Logo251@users.noreply.github.com>
2020-02-22 17:49:09 -0800

    Made methods that did not modify the object constant. Improved documentation.

--- Hash.h
+++ Hash.h
@@ -9,24 +9,25 @@ class Hash {
 public:
    Hash();
    ~Hash();
-   void AddUsingNumber(std::string inputName, int inputNumbers[]);
-   void AddUsingName(std::string inputName, int inputNumbers[]);
-   void AddUsingNameAndNumber(std::string inputName, int inputNumbers[]);
+   const void AddUsingNumber(std::string inputName, int inputNumbers[]);
+   const void AddUsingName(std::string inputName, int inputNumbers[]);
+   const void AddUsingNameAndNumber(std::string inputName, int inputNumbers[]);
    friend std::ostream& operator<<(std::ostream& out, const Hash& hash);
 
 private:
-   long long int ConvertIntArrayToInt(int array[]);
+   const long long int ConvertIntArrayToInt(int array[]);
    void AddToHashTable(std::string name, long long int phoneNumber, long long int position);
 
    struct Node {
       Node(std::string name, long long int number);
 
-      std::string name; //This is the name of the person for the entry given.
-      long long int number;       //This is the phone number of the person in the entry given.
-      Node* nextNode = nullptr;   //This is a pointer to the next node in the chain, if one exists.
+      std::string name;          //This is the name of the person for the entry given.
+      long long int number;      //This is the phone number of the person in the entry given.
+      Node* nextNode = nullptr;  //This is a pointer to the next node in the chain, if one exists.
    };
 
-   int addTime = 0; //The time it takes for a hash function to add all the entries.
+   bool filled = false;   //set to true when something is added to hash. If nothing is added,
+                          //operator<< should behave differently.
    Node* hashTable[4177]; //This is the hash table that nodes will be stored in. It is 4,177
                           //nodes because that is the size the specifications said it must be.
 };
